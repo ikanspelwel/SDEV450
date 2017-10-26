5906758660adcea38bb3d4a559678d6146373ef9
ikanspelwel <ikanspelwel@192.168.0.189>
2017-10-26 18:57:29 -0400

    New Commit.

---
+++ src/java/NewClass.java
@@ -0,0 +1,14 @@
+
+/** 
+ * @Course: SDEV 350 ~ Java Programming II
+ * @Author Name: Adam Ring
+ * @Assignment Name: 
+ * @Date: Oct 26, 2017
+ * @Subclass NewClass Description: 
+ */
+//Imports
+
+//Begin Subclass NewClass
+public class NewClass {
+
+} //End Subclass NewClass
---
+++ src/java/TestClass.java
@@ -0,0 +1,14 @@
+
+/** 
+ * @Course: SDEV 350 ~ Java Programming II
+ * @Author Name: Adam Ring
+ * @Assignment Name: 
+ * @Date: Oct 26, 2017
+ * @Subclass TestClass Description: 
+ */
+//Imports
+
+//Begin Subclass TestClass
+public class TestClass {
+
+} //End Subclass TestClass
