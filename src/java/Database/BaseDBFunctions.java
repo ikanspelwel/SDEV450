90f3ee476373d1ea14ccb9f6ddcbf3608253b4bc
Flammino <Flammino@gmail.com>
2017-11-04 01:09:35 -0400

    Added executeSQL() to allow for execution of SQL statements, getResults() to return result sets

--- src/java/Database/BaseDBFunctions.java
+++ src/java/Database/BaseDBFunctions.java
@@ -1,13 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Adam Flammino 11/3/17
  */
 package Database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
@@ -19,6 +19,8 @@ public class BaseDBFunctions {
     String username;
     String password;
     Connection con;
+    Statement stmt;
+    ResultSet rs;
 
     // Constructor
     BaseDBFunctions(String h, String u, String p) {
@@ -28,7 +30,7 @@ public class BaseDBFunctions {
     }
 
     // Connects to database, returns true if sucessful
-    boolean Connect() {
+    boolean connect() {
         try {
             con = DriverManager.getConnection(host, username, password);
             return true;
@@ -37,5 +39,23 @@ public class BaseDBFunctions {
             System.out.println(err.getMessage());
             return false;
         }
+    }
+    
+    // Executes whatever sql statement is passed in, returns true if sucessful
+    boolean executeSQL(String SQL){
+        try{
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(SQL);
+            return true;
+        }
+        catch (SQLException err){
+            System.out.println(err.getMessage());
+            return false;
+        }
+    }
+    
+    // Returns results set created by ExecuteSQL()
+    ResultSet getResults(){
+        return rs;
     }
 }
