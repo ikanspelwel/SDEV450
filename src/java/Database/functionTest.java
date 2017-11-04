da9d109e8f6719ea8d2bc77c51c03bfc83830014
Flammino <Flammino@gmail.com>
2017-11-04 15:31:44 -0400

    still has sql syntax error?

--- src/java/Database/functionTest.java
+++ src/java/Database/functionTest.java
@@ -11,14 +11,15 @@ public class functionTest {
 
     public static void main(String args[]) {
         boolean flag;
-        String host = "jdbc:mysql://sdev450.gmavt.net:3306/demo1";
+        String host = "jdbc:mysql://sdev450.gmavt.net:3306";
         String username = "demo1";
         String password = "DirectSellDbAccess1234";
         Calendar calendar = Calendar.getInstance();
         java.sql.Date date = new java.sql.Date(calendar.getTime().getTime());
-        String insert = "INSERT INTO Users() VALUES(test1, HamBurglar, "
-                + "0000000000000000000000000000000000000000000000000000000000000"
-                + "000, 1111111111, 12345, 0, " + date;
+        String insert = "insert into demo1.USERS VALUES('test1', 'HamBurglar', "
+                + "0000000000000000000000000000000000000000000000000000000000000000, 1111111111, 12345, 0, " + date;
+        
+        // ("EMAIL, FULL_NAME, PASSWORD, SALT, ZIP, RECOVERY_KEY, DATE_JOINED)
         BaseDBFunctions dbTest = new BaseDBFunctions();
         flag = dbTest.connect(host, username, password);
         if (flag){
