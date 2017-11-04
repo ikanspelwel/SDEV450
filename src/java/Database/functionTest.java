15596054426249b35c1ae9bce9156e7d7edc34a0
Flammino <Flammino@gmail.com>
2017-11-04 17:34:00 -0400

    Added select test

--- src/java/Database/functionTest.java
+++ src/java/Database/functionTest.java
@@ -4,23 +4,25 @@
  */
 package Database;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Calendar;
 
 
 public class functionTest {
 
     public static void main(String args[]) {
+        ResultSet rs;
         boolean flag;
         String host = "jdbc:mysql://sdev450.gmavt.net:3306";
         String username = "demo1";
         String password = "DirectSellDbAccess1234";
         Calendar calendar = Calendar.getInstance();
         String insert = "insert into demo1.USERS(EMAIL, FULL_NAME, PASSWORD, SALT, "
-                + "ZIP, RECOVERY_KEY, DATE_JOINED) VALUES('test3', 'RealName', "
-                + "0000000000000000000000000000000000000000000000000000000000000002, "
-                + "1111111113, 12347, 125, NOW())";
-        
-        // 
+                + "ZIP, RECOVERY_KEY, DATE_JOINED) VALUES('test4', 'RealName1', "
+                + "0000000000000000000000000000000000000000000000000000000000000003, "
+                + "1111111114, 12348, 126, NOW())";
+        String select = "select * from demo1.USERS";
         BaseDBFunctions dbTest = new BaseDBFunctions();
         flag = dbTest.connect(host, username, password);
         if (flag){
@@ -35,6 +37,25 @@ public class functionTest {
         }
         else{
             System.out.println("Insert failed!");
+        }
+        flag = dbTest.executeSQL(select, false);
+        if (flag){
+            System.out.println("Result set selected!");
+            rs = dbTest.getResults();
+            try{
+            while (rs.next()){
+             System.out.printf("%d %s %s %s %s %d %s %s \n", rs.getInt("UID"), rs.getString("EMAIL"),
+                     rs.getString("FULL_NAME"), rs.getString("PASSWORD"), 
+                     rs.getString("SALT"), rs.getInt("ZIP"), rs.getString("RECOVERY_KEY"), 
+                     rs.getDate("DATE_JOINED"));  
+            }
+            }
+            catch (SQLException err){
+            System.out.println(err.getMessage());
+            }
+        }
+        else{
+            System.out.println("Select failed!");
         }
     }
 }
