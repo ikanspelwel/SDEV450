f2bbcff069ac19c75808799561a82e76fc2950d9
farblondzhet <veggietree@gmail.com>
2017-12-02 21:10:34 -0500

    //contact servlet now works; jsp updated

--- src/java/Servlets/Contact.java
+++ src/java/Servlets/Contact.java
@@ -1,11 +1,11 @@
 package Servlets;
 
-/** 
+/**
  * @Course: SDEV 250 ~ Java Programming I
  * @Author Name: Ari
  * @Assignment Name: Database
  * @Date: Nov 9, 2017
- * @Subclass Contact Description: 
+ * @Subclass Contact Description:
  */
 //Imports
 import java.io.*;
@@ -19,61 +19,72 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/Contact")
-public class Contact extends HttpServlet{
+public class Contact extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
-     
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-        
+            throws ServletException, IOException {
+
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        
+
         try {
             String from = request.getParameter("email");
-            String pw = request.getParameter("pw");
             String subj = request.getParameter("subject");
             String msg = request.getParameter("message");
-            if (from != null && subj != null && msg != null){
+            if (from != null && subj != null && msg != null) {
                 Properties properties = new Properties();
-                properties.put("mail.smtp.host", "smtp." + 
-                        from.substring(from.lastIndexOf("@") + 1)); //finds mail server
+                properties.put("mail.smtp.host", "smtp."
+                        + from.substring(from.lastIndexOf("@") + 1)); //finds mail server
                 properties.put("mail.smtp.starttls.enable", "true");
-                properties.setProperty("mail.smtp.auth", "true"); 
-                
+                properties.setProperty("mail.smtp.auth", "true");
+
                 try {
 
                     Session session = Session.getDefaultInstance(properties,
                             new javax.mail.Authenticator() {
                         protected PasswordAuthentication getPasswordAuthentication() {
                             return new PasswordAuthentication(
-                                    from, pw);// Specify the Username and the PassWord
+                                    "sdev450directsell@gmail.com", "sdev4502017pw");// Specify the Username and the PassWord
                         }
                     });
                     MimeMessage message = new MimeMessage(session);
-                    message.setFrom(new InternetAddress(from));
-                    message.addRecipient(Message.RecipientType.TO, new InternetAddress("veggietree@gmail.com"));
-                    message.setSubject(subj);
-                    message.setText(msg);                    
+                    message.setFrom(new InternetAddress("sdev450directsell@gmail.com"));
+                    message.addRecipient(Message.RecipientType.TO, new InternetAddress("sdev450directsell@gmail.com"));
+                    message.setSubject("New Support Message: " + subj);
+                    message.setText(msg);
+                    Transport.send(message);
+
+                    //Sends copy of email to user                    
+                    message.setFrom(new InternetAddress("sdev450directsell@gmail.com"));
+                    message.addRecipient(Message.RecipientType.TO, new InternetAddress(from));
+                    message.setSubject("Thank You for Your Inquiry");
+                    message.setText("Hello there,\n\nThank you for contacting Direct Sell. "
+                            + "We have received the following message from you:\n\n\""
+                            +msg
+                            + "\"\n\nOur Customer Service team will be in touch as soon as possible.\n"
+                            + "\nHave wonderful day!\n\n\nSincerely,\n\n"                           
+                            + "Direct Sell Customer Service\n"                            
+                            + "sdev450directsell@gmail.com");
                     Transport.send(message);
                     out.println("Your email has been sent.");
-                    response.sendRedirect("/DirectSell450/contactSent.jsp");                    
+                    response.sendRedirect("/DirectSell450/contactSent.jsp");
 
                 } catch (MessagingException e) {
                     out.println("Messaging Error:");
                     out.println(e);
-                    e.printStackTrace();                    
-                    
+                    e.printStackTrace();
+
                 }
-                        
-                
+
             }
         } catch (Error e) {
             out.println("Other Error:");
             out.println(e);
-            e.printStackTrace();           
-           
+            e.printStackTrace();
+
+        }
     }
-}
 }
--- web/composeEmail.jsp
+++ web/composeEmail.jsp
@@ -17,13 +17,15 @@
             <div class="row">         
                 <div class="col-xs-12">             
                     <form class="form-horizontal" action="Contact" method="post">
-                        <h2 class="signin-heading">Compose Email</h2>                        
+                        <h2 class="signin-heading">Compose Email</h2>          
+                        <label for="inputEmail" class="control-label">Email</label>
+                        <input type="email" name="email" class="form-control" id="inputEmail" placeholder="Email" required><br>
                         <label for="inputSubject" class="control-label">Subject</label>
-                        <input type="text" class="form-control" id="inputSubject" placeholder="Subject" required><br>
+                        <input type="text" name="subject" class="form-control" id="inputSubject" placeholder="Subject" required><br>
                         <label for="comment" class="control-label">Message:</label>
-                        <textarea class="form-control" rows="5" id="inputComment" placeholder="Message..." required></textarea><br>
-                        <button href="inbox.jsp" class="btn btn-primary btn-lg" type="submit" id="emailSubmit">Submit</button>
-                        <button href="inbox.jsp" class="btn btn-warning btn-lg" type="button" id="emailCancel">Cancel</button>
+                        <textarea class="form-control" name="message" rows="5" id="inputComment" placeholder="Message..." required></textarea><br>
+                        <button href="#" class="btn btn-primary btn-lg" type="submit" id="emailSubmit">Submit</button>
+                        <button href="inbox.jsp" class="btn btn-warning btn-lg" type="button" id="emailCancel">Cancel</button>                       
                     </form><br>                    
                 </div>
             </div>
